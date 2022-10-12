c2d7a970a2df7d51b43afa03f83afda2012f0a37
kaushik <kaushiksaraf91@gmail.com>
2022-10-12 15:59:04 +0530

    tasklist with mongodb-rest methods in progress

--- tasklist-app/server.js
+++ tasklist-app/server.js
@@ -2,18 +2,20 @@ const { urlencoded } = require('express')
 const express = require ('express')
 const mongoose = require ('mongoose')
 const app = express()
+const env = require ('dotenv').config()
 
 app.use(express.json())
 app.use(urlencoded({extented:true}))
 
 
-mongoose.connect('mongodb://localhost/todo')
+console.log(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("connected to MongoDB..."))
 .catch((err) => console.error(err));
 
 
 const taskschema = new mongoose.Schema({
-    id,
+
     task: String,
     checkbox: Boolean,
     taskadded: {type: Date, default: Date.now}
@@ -22,12 +24,23 @@ const taskschema = new mongoose.Schema({
 
 const Task = mongoose.model('Task', taskschema)
 
-function createtask(id,task,checkbox){
+function createTask(task,checkbox){
     const mytask = new Task({
-        id:id,
+        
         task:task,
-        checkbox:false
+        checkbox:checkbox
     })
     
     mytask.save().then( res => console.log(res))
 }
+
+// createTask("Go shopping",true)
+
+ async function getTasks(){
+   const tasklist= await Task.find().sort({taskadded:1}).limit(2)
+   console.log(tasklist)
+}
+getTasks()
+
+
+
